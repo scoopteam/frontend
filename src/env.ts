753de110d552ee80ea2cbc2e0f305b53748cbfb6
Joe Banks <joseph@josephbanks.me>
2020-11-13 13:54:14 +0000

    Remove hcaptcha key from env file

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,3 @@
 export default {
-  API_ROOT: process.env.NODE_ENV === "production" ? "https://scoop.gigalixirapp.com/" : "http://localhost:3000/",
-  HCAPTCHA_KEY: process.env.NODE_ENV === "production" ? process.env.REACT_APP_HCAPTCHA_KEY! : "10000000-ffff-ffff-ffff-000000000001"
+  API_ROOT: process.env.NODE_ENV === "production" ? "https://scoop.gigalixirapp.com/" : "http://localhost:3000/"
 }
