7874e57e5ce83112ff4fd396fcdd8b11aa9fce37
Joe Banks <joseph@josephbanks.me>
2020-11-13 13:54:07 +0000

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -3,56 +3,45 @@
 import NavBar from './components/NavBar';
 import Footer from "./components/Footer";
 
-import { Global, jsx, css } from "@emotion/core";
+import { Global, jsx } from "@emotion/core";
 
 import LandingPage from "./pages/LandingPage";
+import AppHome from "./pages/AppHome";
 
-import colours from "./colours";
+import global from "./global";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+const ROUTES = [
+  {
+    path: "/",
+    Component: LandingPage
+  },
+  {
+    path: "/app",
+    Component: AppHome
+  }
+]
 
 function App() {
-  const global = css`
-    @import url('https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&display=swap');
-
-    body {
-      margin: 0;
-      font-family: 'Alegreya Sans', 'Arial', sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-    }
-    
-    a {
-      text-decoration: none;
-      color: ${colours.azure};
-    }
-    
-    a:hover {
-      text-decoration: underline;
-    }
-    
-    .ReactModal__Overlay {
-        opacity: 0;
-        transition: opacity 400ms ease-in-out;
-    }
-
-    .ReactModal__Overlay--after-open{
-        opacity: 1;
-    }
-
-    .ReactModal__Overlay--before-close{
-        opacity: 0;
-    }
-
-`;
-    
-
   return (
     <div>
       <Global styles={global}/>
-      <div>
+      <Router>
         <NavBar/>
-        <LandingPage/>
+        <Switch>
+          {ROUTES.map(({Component, path}) => (
+            <Route path={path} key={path} exact={true}>
+              <Component/>
+            </Route>
+          ))}
+        </Switch>
         <Footer/>
-      </div>
+      </Router>
     </div>
   );
 }
