a121861b6424742376b58f80f9df51eae1be5516
Joe Banks <joseph@josephbanks.me>
2020-11-17 14:57:14 +0000

    Attempt to load user data when app home is reached

--- src/pages/AppHome.tsx
+++ src/pages/AppHome.tsx
@@ -1,8 +1,33 @@
 /** @jsx jsx */
 import {jsx} from "@emotion/core";
+import { useState, useEffect } from "react";
+import { getCurrentUser } from "../api/users";
 
 export default function AppHome() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    getCurrentUser().then(userData => {
+      setUser(userData)
+    }).catch(() => {
+      setUser("error")
+    })
+  })
+
+  if (!user) {
+    return <div>
+      <h1>Loading...</h1>
+    </div>
+  }
+
+  if (user === "error") {
+    return <div>
+      <h1>Could not load user details</h1>
+      <p>Try reload the page or contact joseph@josephbanks.me if the problem persists</p>
+    </div>
+  }
+
   return <div>
-    <h1>Welcome back user</h1>
+    <h1>Welcome back {user.data.full_name}</h1>
   </div>;
 }
