ffdba07390c5be407dd4572a79265b35935981b7
Joe Banks <joseph@josephbanks.me>
2020-11-24 22:16:52 +0000

    Add copy for no organisations into app home

--- src/pages/AppHome.tsx
+++ src/pages/AppHome.tsx
@@ -5,6 +5,8 @@ import { getCurrentUser } from "../api/users";
 import { useQuery } from "react-query";
 
 import { ServerResponse } from "../api";
+import { getUserOrganisations } from "../api/organisation";
+import NoOrganisations from "../copy/NoOrganisations";
 
 const headerNameStyle = css`
 text-align: center;
@@ -12,9 +14,11 @@ font-size: 3em;
 `
 
 export default function AppHome() {
-  const { isLoading, error, data } = useQuery<ServerResponse, Error>(
-    "userData",
-    () => getCurrentUser()
+  const { isLoading, error, data } = useQuery<ServerResponse[], Error>(
+    "feedData",
+    () => {
+      return Promise.all([getCurrentUser(), getUserOrganisations()])
+    }
   );
 
   if (isLoading) {
@@ -37,10 +41,13 @@ export default function AppHome() {
     );
   }
 
+  const [userData, userOrgs] = data!;
+
   if (data) {
     return (
       <div>
-        <h1 css={headerNameStyle}>Welcome back {data!.data!.full_name}</h1>
+        <h1 css={headerNameStyle}>Welcome back {userData.data!.full_name}</h1>
+        {userOrgs.data!.length === 0 ? <NoOrganisations/> : null }
       </div>
     );
   }
