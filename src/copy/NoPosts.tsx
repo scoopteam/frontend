8eaa63a2c3a05a61322358153362c4d67abbdb42
Joe Banks <joseph@josephbanks.me>
2020-11-24 22:16:44 +0000

    Add copy for no posts

---
+++ src/copy/NoPosts.tsx
@@ -0,0 +1,26 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+
+import { ImSleepy } from "react-icons/im";
+import colours from "../colours";
+
+export default function NoPosts() {
+    return <div css={css`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        align-items: center;
+    `}>
+        <div css={css`
+            background-color: ${colours.lightGrey};
+            padding: 20px;
+            border-radius: 20px;
+            color: #A1A1A1;
+        `}>
+            <ImSleepy css={{fontSize: "5em"}}/>
+            <h2>You're in!</h2>
+            <p>Your feed is empty right now because none of your organisations have made any posts!</p>
+        </div>
+    </div>
+}
