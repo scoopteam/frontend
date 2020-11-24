5b1e999a1044199896d41957d57769aa182b984d
Joe Banks <joseph@josephbanks.me>
2020-11-24 12:54:31 +0000

    Add org method to get user organisations

--- src/api/organisation.ts
+++ src/api/organisation.ts
@@ -1,9 +1,19 @@
+import { Server } from "http";
 import { request, ServerResponse } from ".";
 
 interface CreateOrgData {
     name: string
 }
 
+export interface UserOrg {
+    org: Organisation,
+    permissions: string[]
+}
+
+interface Organisation {
+    name: string,
+    code?: string
+}
 
 export async function createOrganisation(
     data: CreateOrgData
@@ -12,6 +22,15 @@ export async function createOrganisation(
         path: "/org",
         method: "POST",
         data: data,
+    });
+
+    return resp;
+}
+
+export async function getUserOrganisations(): Promise<ServerResponse> {
+    let resp = await request({
+        path: "/org",
+        method: "GET"
     });
 
     return resp;
